c821892482e687ba4b1533a763574024037f931e
Pastrick12 <Edgarpastrana95@gmail.com>
2024-06-08 10:00:33 -0700

    retornando ID de la venta

---
+++ SinTransaction/Commands/VentaDetalleCommands.cs
@@ -0,0 +1,20 @@
+﻿using SinTransaction.Database;
+using SinTransaction.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinTransaction.Commands
+{
+    internal class VentaDetalleCommands
+    {
+        public int GuardarVentaDetalle(VentaDetalle concepto)
+        {
+            
+            return 0;
+        }
+    }
+}
