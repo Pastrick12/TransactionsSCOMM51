54b4ada13bfb2f37858913b6d854156777688ed5
Pastrick12 <Edgarpastrana95@gmail.com>
2024-06-15 09:57:10 -0700

    Insertando Venta Detalle

--- SinTransaction/Commands/VentaDetalleCommands.cs
+++ SinTransaction/Commands/VentaDetalleCommands.cs
@@ -11,10 +11,43 @@ namespace SinTransaction.Commands
 {
     internal class VentaDetalleCommands
     {
-        public int GuardarVentaDetalle(VentaDetalle concepto)
+        public void GuardarVentaDetalle(VentaDetalle concepto)
         {
-            
-            return 0;
+            try
+            {
+                using (SqlConnection con = new SqlConnection(Configuracion.ConnectionString))
+                {
+                    string query = @" insert into VentasDetalle
+                      (Renglon,VentaId,Cantidad,Descripcion,PrecioUnitario,Importe)
+                      values 
+                      (@Renglon,@VentaId,@Cantidad,@Descripcion,@PrecioUnitario,@Importe)";
+
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.CommandType = System.Data.CommandType.Text;
+
+                        cmd.Parameters.AddWithValue("@Renglon", concepto.Renglon);
+                        cmd.Parameters.AddWithValue("@VentaId", concepto.VentaId);
+                        cmd.Parameters.AddWithValue("@Cantidad", concepto.Cantidad);
+                        cmd.Parameters.AddWithValue("@PrecioUnitario", concepto.PrecioUnitario);
+                        cmd.Parameters.AddWithValue("@Descripcion", concepto.Descripcion);
+                        cmd.Parameters.AddWithValue("@Importe", concepto.Importe);
+
+                        con.Open();
+                        int filasAfectadas = cmd.ExecuteNonQuery();
+                        if (filasAfectadas == 0)
+                        {
+                            throw new Exception($"No se pudo insertar concepto de venta renglon({concepto.Renglon})");
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
